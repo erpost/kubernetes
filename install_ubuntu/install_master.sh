cf033b6d61e105a86866cade38b6c89ab1472e20
epost <25180070+erpost@users.noreply.github.com>
2018-11-20 09:36:36 -0500

    commit

--- install_ubuntu/install_master.sh
+++ install_ubuntu/install_master.sh
@@ -2,10 +2,17 @@
 
 ############ run as root user ############
 
+# update packages
+echo "############Updating Packages############"
+apt update
+
 # install Docker
+echo "############Installing Docker############"
 apt install -y docker.io
+systemctl enable docker.service
 
 # set cgroup driver
+echo "############Setting C Group Driver############"
 cat << EOF >> /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"]
@@ -13,20 +20,25 @@ cat << EOF >> /etc/docker/daemon.json
 EOF
 
 # add kubernetes repo key
+echo "############Adding Repo Key############"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-# add sources
+# add kubernetes sources
+echo "############Adding Sources############"
 cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 # update packages
+echo "############Updating Packages############"
 apt update
 
 # install packages
+echo "############Installing Kubernetes############"
 apt install -y kubelet kubeadm kubectl
 
 # initialize kubeadm with flannel
+echo "############Initializing Flannel############"
 kubeadm init --pod-network-cidr=10.244.0.0/16
 
 ############ run as standard user ############
--- install_ubuntu/install_node.sh
+++ install_ubuntu/install_node.sh
@@ -2,28 +2,39 @@
 
 ############ run as root user ############
 
+# update packages
+echo "############Updating Packages############"
+apt update
+
 # install Docker
+echo "############Installing Docker############"
 apt install -y docker.io
+systemctl enable docker.service
 
 # set cgroup driver
+echo "############Setting C Group Driver############"
 cat << EOF >> /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"]
 }
 EOF
 
-# add repo key
+# add kubernetes repo key
+echo "############Adding Repo Key############"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 # add kubernetes sources
+echo "############Adding Sources############"
 cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 # update packages
+echo "############Updating Packages############"
 apt update
 
 # install packages
+echo "############Installing Kubernetes############"
 apt install -y kubelet kubeadm
 
 # add node to master
