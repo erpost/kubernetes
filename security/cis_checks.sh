311f4f6544aab194fa6e77632d4157cb2066c8b2
epost <25180070+erpost@users.noreply.github.com>
2018-11-21 13:19:59 -0500

    commit

--- security/cis_checks.sh
+++ security/cis_checks.sh
@@ -2,29 +2,36 @@
 
 # 1.1.1
 # Ensure that the --anonymous-auth argument is set to false
+echo "############ 1.1.1 ############"
 grep anonymous-auth=true /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # 1.1.2
 # Ensure that the --basic-auth-file argument is not set
+echo "############ 1.1.2 ############"
 grep basic-auth-file /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # 1.1.3
 # Ensure that the --insecure-allow-any-token argument is not set
+echo "############ 1.1.3 ############"
 grep insecure-allow-any-token /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # 1.1.4
 # Ensure that the --kubelet-https argument is set to true
+echo "############ 1.1.4 ############"
 grep kubelet-https /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # 1.1.5
 # Ensure that the --insecure-bind-address argument is not set
+echo "############ 1.1.5 ############"
 grep insecure-bind-address /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # 1.1.6
 # Ensure that the --insecure-port argument is set to 0
+echo "############ 1.1.6 ############"
 grep insecure-port /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # 1.1.7
 # Ensure that the --secure-port argument is not set to 0
+echo "############ 1.1.7 ############"
 grep \-secure-port /etc/kubernetes/manifests/kube-apiserver.yaml
 
